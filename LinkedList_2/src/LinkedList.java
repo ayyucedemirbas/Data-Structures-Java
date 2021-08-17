e91f79c50eb3d132eba9edb0deda7a880b76e3c7
Ayyuce Demirbas <a.ayyuced@gmail.com>
2021-08-18 01:49:45 +0300

    Update LinkedList.java

--- LinkedList_2/src/LinkedList.java
+++ LinkedList_2/src/LinkedList.java
@@ -74,6 +74,63 @@ public class LinkedList {
 		
 	}
 	
+    void deleteNode(int key){
+        // Store head node
+        Node temp = head, prev = null;
+ 
+        // If head node itself holds the key to be deleted
+        if (temp != null && temp.data == key) {
+            head = temp.next; // Changed head
+            return;
+        }
+ 
+        // Search for the key to be deleted, keep track of
+        // the previous node as we need to change temp.next
+        while (temp != null && temp.data != key) {
+            prev = temp;
+            temp = temp.next;
+        }
+ 
+        // If key was not present in linked list
+        if (temp == null)
+            return;
+ 
+        // Unlink the node from linked list
+        prev.next = temp.next;
+    }
+	
+	
+	
+    void deleteNode_position(int position){
+        // If linked list is empty
+        if (head == null)
+            return;
+ 
+        // Store head node
+        Node temp = head;
+ 
+        // If head needs to be removed
+        if (position == 0)
+        {
+            head = temp.next;   // Change head
+            return;
+        }
+ 
+        // Find previous node of the node to be deleted
+        for (int i=0; temp!=null && i<position-1; i++)
+            temp = temp.next;
+ 
+        // If position is more than number of nodes
+        if (temp == null || temp.next == null)
+            return;
+ 
+        // Node temp->next is the node to be deleted
+        // Store pointer to the next of node to be deleted
+        Node next = temp.next.next;
+ 
+        temp.next = next;  // Unlink the deleted node from list
+    }
+	
     public void printList()
     {
         Node tnode = head;
